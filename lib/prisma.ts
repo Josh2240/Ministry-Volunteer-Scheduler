7bff154c0c9097c1dacd5b956bea335a0f1b90a8
Josh <joshuacabrailla009897@gmail.com>
2026-08-19 16:34:49 +0800

    to be continued

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,21 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { PrismaClient } from "@prisma/client";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
+  console.warn("DATABASE_URL is not set");
 }
 
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: connectionString,
+      url: connectionString || "",
     },
   },
 });
