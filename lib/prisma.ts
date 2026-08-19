425e6ece9ea04cb4b8123dbc4069702e0824e31b
Josh <joshuacabrailla009897@gmail.com>
2026-08-19 15:15:49 +0800

    fixed prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = globalThis as unknown as { prisma: PrismaClient };
+const connectionString = process.env.DATABASE_URL;
 
-export const prisma = prismaClient.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  prismaClient.prisma = prisma;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
 }
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: connectionString,
+    },
+  },
+});
